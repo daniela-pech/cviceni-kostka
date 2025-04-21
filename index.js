3733877591878106a18682342388fe01e3964c2f
Daniela Pech <randakova.daniela@gmail.com>
2025-04-21 17:32:25 +0200

    vypracovane cviceni udalosti, keydown, if

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+let viditelneCislo = 1;
+const kostka = document.querySelector('.dice');
+const hod = () => {
+  viditelneCislo = viditelneCislo + 1;
+  if (viditelneCislo === 7) {
+    viditelneCislo = 1;
+  }
+  kostka.src = `img/side${viditelneCislo}.svg`;
+};
+document.body.addEventListener('keydown', hod);
